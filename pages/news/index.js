c3acf27685b8edc4a11ddd8e16d73216a6f9aa98
Maciej-Koc <mkoc2@asu.edu>
2022-08-03 11:36:06 -0500

    Completed module.

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,3 +1,19 @@
+import Link from "next/link";
+
 export default function News() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href={`/news/a-great-framework`}>
+            NextJS is a great framework
+          </Link>
+        </li>
+        <li>
+          <Link href={`/news/`}>Something Else</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
